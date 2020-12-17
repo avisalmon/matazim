de437d2850ffdd244513addf78d788c7f0d816f9
Salmon <>
2020-12-17 07:37:09 +0200

    adding course published field

--- learn/models.py
+++ learn/models.py
@@ -16,6 +16,7 @@ class Course(models.Model):
     predecessor = models.ManyToManyField("self", blank=True, symmetrical=False)
     youtube = models.CharField(max_length=200, blank=True)
     members = models.ManyToManyField(get_user_model(), through='Registration', related_name='members')
+    published = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
